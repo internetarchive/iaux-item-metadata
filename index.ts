420a445b3139b6925c730d75a7b942964eea059a
Jason Buckner <jason@jasonbuckner.com>
2025-01-09 21:55:49 -0800

    Export additional list fields

--- index.ts
+++ index.ts
@@ -9,12 +9,17 @@ export { BooleanField } from './src/models/metadata-fields/field-types/boolean';
 export { ByteField } from './src/models/metadata-fields/field-types/byte';
 export { DateField } from './src/models/metadata-fields/field-types/date';
 export { DurationField } from './src/models/metadata-fields/field-types/duration';
-export { ListField } from './src/models/metadata-fields/field-types/list';
+export {
+  ListField,
+  StringListField,
+  NumberListField,
+} from './src/models/metadata-fields/field-types/list';
 export { MediaTypeField } from './src/models/metadata-fields/field-types/mediatype';
 export { NumberField } from './src/models/metadata-fields/field-types/number';
 export { PageProgressionField } from './src/models/metadata-fields/field-types/page-progression';
 export { StringField } from './src/models/metadata-fields/field-types/string';
 
+// base metadata field models
 export {
   MetadataFieldInterface,
   MetadataRawValue,
