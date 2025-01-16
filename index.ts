c92def05353003469a280d2ca44562a10cf1dc3a
Jason Buckner <jason@jasonbuckner.com>
2025-01-16 14:01:45 -0800

    Remove File and Review objects
    They're specific to the metadata-service response

--- index.ts
+++ index.ts
@@ -1,8 +1,5 @@
 // top level models
-export { File } from './src/models/file';
 export { Metadata } from './src/models/metadata';
-export { Review } from './src/models/review';
-export { SpeechMusicASREntry } from './src/models/speech-music-asr-entry';
 
 // metadata field types
 export { BooleanField } from './src/models/metadata-fields/field-types/boolean';
